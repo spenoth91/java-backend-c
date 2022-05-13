c08d8da7460d1149c4ee4c75495ddf16abc2e365
Gorea Tudor <tudortudor2002@gmail.com>
2022-05-13 14:20:56 +0300

    added validateUserTO

--- src/main/java/com/msglearning/javabackend/services/UserService.java
+++ src/main/java/com/msglearning/javabackend/services/UserService.java
@@ -28,6 +28,26 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public static boolean validateUser (User user) {
+        return true;
+    }
+
+    public static boolean validateUserTO (UserTO userTO) {
+        if (userTO.getEmail() == null || !userTO.getEmail().matches("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$"))
+            return false;
+
+        if (userTO.getFirstName() == null || userTO.getLastName() == null)
+            return false;
+
+        if (!userTO.getPhone().matches("^(\\+4|)?(07[0-8]{1}[0-9]{1}|02[0-9]{2}|03[0-9]{2}){1}?(\\s|\\.|\\-)?([0-9]{3}(\\s|\\.|\\-|)){2}$"))
+            return false;
+
+        if (userTO.getHashedPWD() == null)
+            return false;
+
+        return true;
+    }
+
     public List<UserTO> findAll() {
         List<User> users = userRepository.findAll();
         if (users.isEmpty())
@@ -51,7 +71,6 @@ public class UserService {
     public Optional<User> findById(Long id) {
         return userRepository.findById(id);
     }
-
 
     public Optional<User> findByEmail(String email) {
         return userRepository.findByEmail(email);
