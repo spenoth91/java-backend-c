43284fa07cbcab0b243830d9eda78fb19afa7a2c
Gorea Tudor <tudortudor2002@gmail.com>
2022-05-19 20:22:08 +0300

    Employee: simple GET & POST
    added EmployeeController class
    added GET all
    added GET by id
    added POST new employee

--- src/main/java/com/msglearning/javabackend/repositories/EmployeeRepository.java
+++ src/main/java/com/msglearning/javabackend/repositories/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.msglearning.javabackend.repositories;
 import com.msglearning.javabackend.entity.Employee;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends CrudRepository <Employee, Long> {
-    // code here
+
+    List<Employee> findAll();
 }
