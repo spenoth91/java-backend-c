68ba1084f7335c42cf679900028eabeca27ae4c0
Gorea Tudor <tudortudor2002@gmail.com>
2022-05-19 18:30:11 +0300

    added Person model - NOT TESTED
    contains: id, employeeID, password, fullName, address, email, phone

---
+++ src/main/java/com/msglearning/javabackend/entity/Person.java
@@ -0,0 +1,35 @@
+package com.msglearning.javabackend.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Table(name = Person.TABLE_NAME)
+@Entity
+public class Person {
+
+    static final String TABLE_NAME = "person";
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "emp_id", unique = true) // nullable = true (default)
+    private Long employeeID;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+}
